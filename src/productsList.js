acf7b9f45b34ab92a7e61474ffd0936dfa5c5631
Christian Gomez <cjgm14@gmail.com>
2020-03-05 13:56:06 -0600

    Obtener un producto y venta por producto

--- src/productsList.js
+++ src/productsList.js
@@ -4,18 +4,21 @@ export default [
     id: uuidv4(),
     name: 'Coca cola',
     stock: 10,
-    value: 12
+    value: 12,
+    sold: 0
   },
   {
     id: uuidv4(),
     name: 'Carne molida',
     stock: 20,
-    value: 60
+    value: 60,
+    sold: 0
   },
   {
     id: uuidv4(),
     name: 'Sabritas',
     stock: 5,
-    value: 15
+    value: 15,
+    sold: 0
   }
 ];
